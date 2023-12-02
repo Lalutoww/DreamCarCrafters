18b9964ce305729679b20b67216e77ecec963752
Martin Lalutov <martinlalutov00@gmail.com>
2023-12-02 13:57:54 +0200

    [DCC] fix bug

--- client/src/components/pages/car/browse-cars/BrowseCars.jsx
+++ client/src/components/pages/car/browse-cars/BrowseCars.jsx
@@ -23,7 +23,6 @@ const BrowseCars = () => {
       }
    }, [allCars]);
    if (showNoCars) {
-      throw new Error('NOOO!')
       return (
          <div className={styles['no-cars']}>There are no cars for sale!</div>
       );
